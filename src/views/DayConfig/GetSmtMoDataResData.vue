84904926f60133ffd5922bc2d29517011366aa1c
FHY <51120966+fuhychn@users.noreply.github.com>
2023-11-03 12:34:33 +0800

    Update GetSmtMoDataResData.vue

--- src/views/DayConfig/GetSmtMoDataResData.vue
+++ src/views/DayConfig/GetSmtMoDataResData.vue
@@ -777,7 +777,7 @@ export default {
     // 获取上传文件
     handleChange(file, fileList) {
       const fileName = file.name.replace(/\.xlsx$/, '')
-      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(AI|点胶)(正排|预排).*$/
+      const regex = /^(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(AI|点胶|主板|小板)(正排|预排).*$/
       if (!regex.test(fileName)) {
         const tip = '文件命名格式错误，请修改后重新上传！' + `<br/>` + '（正确文件名示例：0901点胶预排）'
         this.$alert(tip, '错误', {
